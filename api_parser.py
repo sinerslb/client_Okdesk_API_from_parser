ce59f34f10f2a5359d2fc2a4bb8e97918497b0ab
Алексей Андреев <siner1982@gmail.com>
2025-09-29 00:47:47 +0500

    feat: Info collection from endpoint descriptions

--- api_parser.py
+++ api_parser.py
@@ -7,6 +7,7 @@ import requests
 from bs4 import BeautifulSoup
 from bs4.element import NavigableString, PageElement, ResultSet, Tag
 
+Description = list[list[str | list[str]] | tuple[str, ...]]
 Result_set = ResultSet[PageElement | Tag | NavigableString]
 
 
@@ -53,17 +54,104 @@ def get_elements_of_class_tag(
     ]
 
 
+def normalize_str(
+        string: str
+) -> str:
+    """Clear a line of extra spaces, line breaks, and ¶.
+
+    Args:
+        string (str): string
+
+    Returns:
+        str: a string without spaces or line breaks
+    """
+    words = string.split()
+    string = ' '.join(words)
+    return string.replace(' ¶', '')
+
+
+def get_data_from_note(
+        note: Tag
+) -> tuple[str, ...]:
+    """Get data from a note.
+
+    Args:
+        note (Tag): web element, class 'note'
+
+    Returns:
+        list[str]: a list with the tag name "note" at position zero,
+            and the table data
+    """
+    result: list[str] = ['note']
+    note_texts = [normalize_str(element.text) for element in note.children]
+    result.extend(
+        [text for text in note_texts if text != '']
+    )
+    return tuple(result)
+
+
+def get_data_from_table(
+        table: Tag
+) -> list[str | list[str]]:
+    """Get data from a table.
+
+    Args:
+        table (Tag): web element <table>
+
+    Returns:
+        list[str | list[str]]: a list with the tag name "table"
+            at position zero, and the table data
+    """
+    table_data: list[str | list[str]] = ['table']
+    table_rows = table.find_all('tr')
+    for row in get_elements_of_class_tag(table_rows):
+        cells = row.find_all(['td', 'th'])
+        row_data = [normalize_str(cell.text) for cell in cells]
+        table_data.append(row_data)
+    return table_data
+
+
+def parse_description(
+        ep_children: list[Tag]
+) -> Description:
+    """Collects information from the endpoint description.
+
+    Args:
+        ep_children (list[Tag]): list of web elements from
+            the endpoint documentation
+
+    Returns:
+        Description: a list of information from the 'p', 'h4', 'table'
+        and 'note' tags, up to the 'h4' tag with the text URI example.
+    """
+    desc_data: Description = []
+    for ep_child in ep_children:
+        if ep_child.name == 'p':
+            desc_data.append(('p', normalize_str(ep_child.text)))
+        elif ep_child.name == 'h4':
+            h4_text: str = ep_child.text
+            if h4_text.find('URI') >= 0:
+                break
+            else:
+                desc_data.append(('h4', normalize_str(h4_text)))
+        elif ep_child.name == 'table':
+            desc_data.append(get_data_from_table(ep_child))
+        elif 'note' in ep_child.attrs.get('class', ''):
+            desc_data.append(get_data_from_note(ep_child))
+    return desc_data
+
+
 def parse_a_section(
         section_element: Tag,
         dict_links_to_docs: dict
-) -> dict[str, dict[str, str | list[list[str | list[str]]]]]:
+) -> dict[str, dict[str, str | Description]]:
     """Parses section.
 
     Args:
         section_element (Tag): web element 'section'
         section_data (dict[str, dict[str, str]]): dictionary with sections data
     """
-    section_data: dict[str, dict[str, str | list[list[str | list[str]]]]] = {}
+    section_data: dict[str, dict[str, str | Description]] = {}
     resources = get_elements_of_class_tag(
         section_element.find_all(class_='resource')
     )
@@ -87,14 +175,14 @@ def parse_a_section(
             endpoint_data['link_to_the_doc'] = link_to_doc
             endpoint_data['method'] = ep_url_data[0].text
             endpoint_data['uri'] = ep_url_data[1].text
-            endpoint_data['description'] = []
+            endpoint_data['description'] = parse_description(ep_children[2:])
     return section_data
 
 
 def get_okdesk_api_data(
             content: Tag,
             dict_links_to_docs: dict
-) -> dict[str, dict[str, dict[str, str | list[list[str | list[str]]]]]]:
+) -> dict[str, dict[str, dict[str, str | Description]]]:
     api_data = {}
     sections = get_elements_of_class_tag(
         content.find_all('section')
