e806f77f4b4cd703ae1d7ca49f895cb22d59306d
Алексей Андреев <99258799+sinerslb@users.noreply.github.com>
2025-10-07 17:08:29 +0500

    Revert "Feat: the main function of parsing"
    This reverts commit ad00653c57958f2fd39adb8f3d3e9fe5e5a8e84f.

--- api_parser.py
+++ api_parser.py
@@ -1,5 +1,3 @@
-import json
-import requests
 from urllib.parse import urldefrag
 
 
@@ -16,39 +14,3 @@ def get_base_url(
     """
     defrag_url = urldefrag(url)
     return defrag_url.url
-
-
-def get_parsed_api_data(
-    api_docs_url: str = 'https://apidocs.okdesk.com/apidoc'
-) -> dict:
-    """Return parsed documentation data.
-
-    Args:
-        api_docs_url (str, optional): Link to documentation site Okdesk API.
-
-    Returns:
-        dict: Okdesk API documentation data dictionary
-    """
-    api_data = {}
-    base_url = get_base_url(api_docs_url)
-    try:
-        with requests.Session() as session:
-            okdesk_api_site = session.get(
-                base_url
-            )
-        okdesk_api_site.raise_for_status()
-    except Exception as ex:
-        api_data['error'] = str(ex)
-    return api_data
-
-
-if __name__ == '__main__':
-    okdesk_api_data = get_parsed_api_data()
-    with open('okdesk_api_data.json', 'w', encoding='utf-8') as f:
-        json.dump(
-            okdesk_api_data,
-            f,
-            ensure_ascii=False,
-            indent=4
-        )
-    print('Ready!')
